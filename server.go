cf990fb2e9e62856bf1aa9d6744d3569621cfdda
yogipristiawan <yogistanada252@gmail.com>
2022-11-12 19:35:56 +0700

    initial commit

---
+++ server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"graphql/graph"
+	"graphql/graph/generated"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+)
+
+const defaultPort = "8080"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
